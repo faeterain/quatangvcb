ddbfb3b727cb28a39768b20ec49dae74d801d1af
Phat Nguyen <tanphat.uit@gmail.com>
2022-09-28 00:19:46 +0700

    update table

--- src/App.js
+++ src/App.js
@@ -4,20 +4,168 @@ import { Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import QuyDoi from './pages/QuyDoi';
 import XacNhan from './pages/XacNhan';
+import { AppBar, Box, Container, IconButton, Toolbar, Typography, Avatar, Button, Tooltip } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import React from 'react';
+import AdbIcon from '@mui/icons-material/Adb';
+
+const pages = [
+  {name: 'Trang chủ', url: '/quatangvcb'},
+  {name: 'Quy Đổi', url: '/quatangvcb/quydoi'},
+  {name: 'Xác Nhận', url: '/quatangvcb/xacnhan'}
+];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 function App() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
   return (
     <div className="App">
-      <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/quydoi'>Quy Doi</Link></li>
-        <li><Link to='/xacnhan'>Approve</Link></li>
-      </ul>
+      <AppBar position="absolute" >
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page, index) => (
+                <MenuItem key={index} onClick={handleCloseNavMenu}>
+                  <Link className='menu-link' to={page.url}><Typography textAlign="center">{page.name}</Typography></Link>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page, index) => (
+              <Button
+                key={index}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                <Link className='menu-link' to={page.url}>{page.name}</Link>
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+        </AppBar>
+      <div style={{marginTop: 90}}></div>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/quydoi' element={<QuyDoi />} />
-        <Route path='/xacnhan' element={<XacNhan />} />
+        <Route path='/quatangvcb' element={<Home />} />
+        <Route path='/quatangvcb/quydoi' element={<QuyDoi />} />
+        <Route path='/quatangvcb/xacnhan' element={<XacNhan />} />
       </Routes>
+      
     </div>
   );
 }
